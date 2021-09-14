639bd6d67d63e498705044bbfbb83c85e138bd9c
clizioguedes <clizioguedes@gmail.com>
2021-09-13 23:19:06 -0300

    Add title into document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Main, NextScript } from "next/document";
+// eslint-disable-next-line @next/next/no-head-import-in-document
+import Head from "next/head";
 export default class MyDocument extends Document {
   render() {
     return (
